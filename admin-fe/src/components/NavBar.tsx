982cc67744dcb3d1fbedf02ddf553f0bbfc9d6b9
Bilguun.B <zolbooenkhbat@MacBook-Air.local>
2024-03-19 19:36:39 +0800

    do admin fe addProductBar

---
+++ admin-fe/src/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { IconBell } from "@/svg/IconBell";
+import { IconPinecon } from "@/svg/IconPinecon";
+import { IconUser } from "@/svg/IconUser";
+
+export const NavBar = () => {
+  return (
+    <div className="flex bg-black py-1 px-6 justify-between h-12 items-center">
+      <div>
+        <a href="">
+          <IconPinecon />
+        </a>
+      </div>
+      <div className="flex items-center gap-5 items-center">
+        <div>
+          <a href="">
+            <IconBell />
+          </a>
+        </div>
+        <div className="flex gap-4 items-center">
+          <a href="">
+            <IconUser />
+          </a>
+          <button className="text-white">Username</button>
+        </div>
+      </div>
+    </div>
+  );
+};
