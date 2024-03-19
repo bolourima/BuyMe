aada81457f3b9890c992e36201911a490282324e
Bolormaa Ihkbayar <23LP2875@pinecones-MacBook-Air-2.local>
2024-03-19 20:03:33 +0800

    Hero html css

--- user-fe/src/pages/index.tsx
+++ user-fe/src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Hero } from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 }
