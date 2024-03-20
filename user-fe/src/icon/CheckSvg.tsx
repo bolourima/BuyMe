ecb5bda5f9c4db51c032ca5d9047b247a59964a0
Bolormaa Ihkbayar <23LP2875@pinecones-MacBook-Air-2.local>
2024-03-20 19:38:22 +0800

    product detail all

---
+++ user-fe/src/icon/CheckSvg.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const CheckSvg = () => {
+  return (
+    <div>
+      <svg
+        width="16"
+        height="12"
+        viewBox="0 0 16 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M5.32923 9.22914L1.85423 5.75414L0.670898 6.92914L5.32923 11.5875L15.3292 1.58748L14.1542 0.412476L5.32923 9.22914Z"
+          fill="#8B96A5"
+        />
+      </svg>
+    </div>
+  );
+};
