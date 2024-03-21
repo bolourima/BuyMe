91bb81561be11230782310ebcfc52efb86e3c9b5
Bilguun.B <zolbooenkhbat@MacBook-Air.local>
2024-03-21 17:46:49 +0800

    income

--- admin-fe/src/pages/index.tsx
+++ admin-fe/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { AddProductBar } from "@/components/AddProductBar";
 import Income from "./income";
-import Product from "./product";
 
 export default function Home() {
   return (
     <>
       <div>
+        {/* <AddProductBar /> */}
         {/* <AddProductBar />
         <Product /> */}
         <Income />
