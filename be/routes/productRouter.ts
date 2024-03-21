0016a9d48c1949ec6b8d5278ec67f1a4398a8e53
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-03-21 17:35:29 +0800

    back-end getProducts route bolon function

--- be/routes/productRouter.ts
+++ be/routes/productRouter.ts
@@ -1,2 +1,4 @@
 import { Router } from "express";
+import { getProducts } from "../controllers/productController";
 export const productRouter = Router();
+productRouter.route("/getProducts").get(getProducts);
