626a14d1294c2d44fea862c57fd96de26953b3a5
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-03-22 10:56:33 +0800

    admin-fe componentuudin hamaaral ajillga

---
+++ admin-fe/src/types/getProductType.ts
@@ -0,0 +1,16 @@
+import { Category } from "./categoryType";
+
+export type GetProductType = {
+  name: string;
+  description: string;
+  price: number;
+  productCode: number;
+  quantity: number;
+  tag: string;
+  disCount: { isSale: boolean; salePercent: number };
+  categoryId: Category;
+  subCategoryName: string;
+  brandName: string;
+  createdAt: string;
+  images: [];
+};
