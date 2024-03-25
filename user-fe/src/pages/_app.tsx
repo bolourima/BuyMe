9b53a44f01d35f99543f30ad6a1e2cb31aa0cfb1
Bolormaa Ihkbayar <23LP2875@pinecones-MacBook-Air-2.local>
2024-03-25 21:04:59 +0800

    delete signin signup component

--- user-fe/src/pages/_app.tsx
+++ user-fe/src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
-import SignUp from "../components/SignUp";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
