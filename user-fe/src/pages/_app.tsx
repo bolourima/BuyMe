f93ec695f571a66a98332860d6b62381e2f6bd7b
lkhagvace <147494260+lkhagvace@users.noreply.github.com>
2024-03-26 00:13:39 +0800

    68 user fe product map hiij hargddg blgh (#73)
    * product hardaga bolson mun sagsaldg bolson
    
    * sagsaldg bolsn
    
    ---------
    
    Co-authored-by: Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>

--- user-fe/src/pages/_app.tsx
+++ user-fe/src/pages/_app.tsx
@@ -1,7 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
+import { BasketBarVisiblity } from "@/context/BasketVisiblity";
+import { ProductsInBasket } from "@/context/FoodsInBasket";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ProductsInBasket>
+      <BasketBarVisiblity>
+        <Component {...pageProps} />
+      </BasketBarVisiblity>
+    </ProductsInBasket>
+  );
 }
