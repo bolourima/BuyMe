efda3e2bd414477b2df01f90cf8f8126046c8ea1
unknown <bumanhuslenb@gmail.com>
2024-03-26 17:56:16 +0800

    fix product map

--- user-fe/src/pages/index.tsx
+++ user-fe/src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Product from "../components/Product";
 import { instance } from "../instance";
 import { ProductType } from "../types/productType";
+import ProductDetail from "./productdetail";
 
-export default function Home() {
+export default function Home({ productData }: { productData: ProductType[] }) {
   return (
     <div className="bg-white p-4 m-10 rounded-lg ">
       <a href="/productList" className="">
