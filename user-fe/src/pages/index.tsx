f3d1e1c910e4d0ada38b2511d19e031902b75bce
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-03-27 18:30:42 +0800

    basket page hiiw

--- user-fe/src/pages/index.tsx
+++ user-fe/src/pages/index.tsx
@@ -1,7 +1,4 @@
-import Product from "../components/Product";
-import { instance } from "../instance";
 import { ProductType } from "../types/productType";
-import ProductDetail from "./productdetail";
 
 export default function Home({ productData }: { productData: ProductType[] }) {
   return (
