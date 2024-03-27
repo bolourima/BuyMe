ecbf3d8aff77f8da686cd900386d58c72083e060
unknown <bumanhuslenb@gmail.com>
2024-03-27 19:16:11 +0800

    test

--- user-fe/src/pages/productId/[id].tsx
+++ user-fe/src/pages/productId/[id].tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { instance } from "@/instance";
 
 export default function ProductId() {
   const router = useRouter();
   console.log(router);
   return <div>{router.query._id}</div>;
 }
+export const getServerSideProps = async () => {
+  const productRes = await instance.get("/getProducts");
+  const productData = productRes.data;
+  return {
+    props: { productData },
+  };
+};
