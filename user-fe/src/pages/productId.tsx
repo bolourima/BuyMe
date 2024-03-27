b6a7287d37cd3b7a6a6afafd694a0ee0f9e5983c
unknown <bumanhuslenb@gmail.com>
2024-03-27 19:22:20 +0800

    bumaa

--- user-fe/src/pages/productId.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useRouter } from "next/router";
-
-export default function Product() {
-  const router = useRouter();
-  console.log(router);
-  return <div>{router.query._id}</div>;
-}
