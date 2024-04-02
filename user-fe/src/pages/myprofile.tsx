3e02d909b54ca3f9a243ed6b1b589f8c4173d361
Bilguun.B <zolbooenkhbat@MacBook-Air.local>
2024-04-02 19:04:40 +0800

    update signin signup

--- user-fe/src/pages/myprofile.tsx
+++ user-fe/src/pages/myprofile.tsx
@@ -16,59 +16,6 @@ export default function MyProfile() {
             <ProfileSideBar />
           </div>
 
-          {/* <div className="w-[300px] lg:flex">
-            <div className=" border-gray-200 lg:border-gray-200 border-[1px]">
-              <div className="flex gap-5 items-center p-5">
-                <div className="flex w-12 h-12">
-                  <img
-                    className="rounded-full"
-                    src="./IconPicture.jpeg"
-                    alt=""
-                  />
-                </div>
-                <div className="">
-                  <div className="flex gap-3">
-                    <div>
-                      <p>Hello</p>
-                    </div>
-                    <div className="w-5 h-5">
-                      <HandIcon />
-                    </div>
-                  </div>
-                  <div className="text-xl">
-                    <p>Elizabeth Warren</p>
-                  </div>
-                </div>
-              </div>
-              <div className="">
-                <button className="bttn flex gap-4 bg-white text-black w-full h-[50px] items-center hover:bg-black hover:text-white">
-                  <div className="w-5 h-5 mx-4">
-                    <UserIcon />
-                  </div>
-
-                  <p>Personal Information</p>
-                </button>
-                <button className="bttn flex gap-4 bg-white text-black w-full h-[50px] items-center hover:bg-black hover:text-white">
-                  <div className="w-5 h-5 mx-4">
-                    <CartIcon />
-                  </div>
-                  <p>My Orders</p>
-                </button>
-                <button className="bttn flex gap-4 bg-white text-black w-full h-[50px] items-center hover:bg-black hover:text-white">
-                  <div className="w-5 h-5 mx-4">
-                    <LocationIcon />
-                  </div>
-                  <p>Manage Addresses</p>
-                </button>
-                <button className="bttn flex gap-4 bg-white text-black w-full h-[50px] items-center hover:bg-black hover:text-white">
-                  <div className="w-5 h-5 mx-4">
-                    <NotificationIcon />
-                  </div>
-                  <p>Notifications</p>
-                </button>
-              </div>
-            </div>
-          </div> */}
           <div className="flex flex-col gap-6 w-3/4">
             <div className="flex justify-between w-full px-5">
               <div>
