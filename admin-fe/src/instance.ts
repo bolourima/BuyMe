a70f018352f2fc8f6e128f74d4e571884f658fa5
Bolormaa Ihkbayar <23LP2875@pinecones-MacBook-Air-2.local>
2024-04-04 16:23:51 +0800

    hi

--- admin-fe/src/instance.ts
+++ admin-fe/src/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://buyme-x6zl.onrender.com",
   timeout: 8000,
   withCredentials: true,
 });
