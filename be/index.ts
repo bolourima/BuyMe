148094b0f5993d7ffbae09b281f123e2928eccfc
lkhagvace <147494260+lkhagvace@users.noreply.github.com>
2024-04-04 16:44:51 +0800

    edit cors (#166)
    Co-authored-by: Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>

--- be/index.ts
+++ be/index.ts
@@ -19,8 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
-    credentials: true,
+    origin: "*",
   })
 );
 app.use(categoryRouter);
