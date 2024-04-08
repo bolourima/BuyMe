1cfe7e13b7072c75da2651d93a7feceb255fcfe5
Bo <135795277+bolourima@users.noreply.github.com>
2024-04-08 19:47:41 +0800

    186 myprofile page zasah (#206)
    * update
    
    * m
    
    * update
    
    ---------
    
    Co-authored-by: Bilguun.B <zolbooenkhbat@MacBook-Air.local>

--- user-fe/src/components/ProfileSideBar.tsx
+++ user-fe/src/components/ProfileSideBar.tsx
@@ -5,9 +5,11 @@ import CartIcon from "@/icon/CartIcon";
 import LocationIcon from "@/icon/LocationIcon";
 import NotificationIcon from "@/icon/NotificationIcon";
 import { UserType } from "@/types/userType";
+import { useRouter } from "next/router";
 export const ProfileSideBar = ({ user }: { user: UserType }) => {
+  const router = useRouter();
   return (
-    <div className=" border-gray-200 lg:border-gray-200 border-[1px]">
+    <div className=" border-gray-200 lg:border-gray-200 border-[1px] p-5 rounded-md">
       <div className="flex gap-5 items-center p-5">
         <div className="flex w-12 h-12">
           <img className="rounded-full" src={user.avatarImg} alt="" />
@@ -34,7 +36,10 @@ export const ProfileSideBar = ({ user }: { user: UserType }) => {
 
           <p>Personal Information</p>
         </button>
-        <button className="bttn flex gap-4 bg-white text-black w-full h-[50px] items-center hover:bg-black hover:text-white">
+        <button
+          onClick={() => router.push("/order")}
+          className="bttn flex gap-4 bg-white text-black w-full h-[50px] items-center hover:bg-black hover:text-white"
+        >
           <div className="w-5 h-5 mx-4">
             <CartIcon />
           </div>
