7525025cfafb26b15ee3b3247d5de36c8f9f17dd
Totorosato <77751259+Totorosato@users.noreply.github.com>
2024-04-08 19:52:14 +0800

    198 admin jump connection (#207)
    * fix-page-jump-toastify-product-actions
    
    * fixed data table
    
    ---------
    
    Co-authored-by: Bumanhuslen Erdenebaatar <23LP6507@G304-CC0042.local>

---
+++ admin-fe/src/pages/product.tsx
@@ -0,0 +1,45 @@
+import { MainProducts } from "@/components/MainProducts";
+import { TokenContext } from "@/contexts/TokenContext";
+import { instance } from "@/instance";
+import { Category } from "@/types/categoryType";
+import { GetProductType } from "@/types/getProductType";
+import { OrderType } from "@/types/orderType";
+import { getOrders } from "@/utilities/getOrders";
+import { getProducts } from "@/utilities/getProducts";
+import React, { useContext, useEffect, useState } from "react";
+
+export default function product({
+  categoryData,
+}: {
+  categoryData: Category[];
+}) {
+  const [visibleComponent, setVisibleComponent] = useState("");
+  const [orderData, setOrderData] = useState<OrderType[]>([]);
+  const { token, setToken } = useContext(TokenContext);
+  const [productData, setProductData] = useState<GetProductType[]>([]);
+  const setProduct = (data: GetProductType[]) => {
+    setProductData(data);
+  };
+  const setOrder = (data: OrderType[]) => {
+    setOrderData(data);
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken");
+    if (!token) return;
+    setToken(token);
+    getProducts(token, setProduct);
+    getOrders(token, setOrder);
+  }, []);
+  return (
+    <div className="">
+      <MainProducts categoryData={categoryData} productData={productData} />
+    </div>
+  );
+}
+export const getServerSideProps = async () => {
+  const categoryRes = await instance.get("/getCategories");
+  const categoryData = categoryRes.data;
+  return {
+    props: { categoryData },
+  };
+};
---
+++ admin-fe/src/utilities/toastify.ts
@@ -0,0 +1,44 @@
+import React from "react";
+import { toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const toastifySuccess = (text: String) => {
+  toast.success(text, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+};
+export const toastifyError = (text: String) => {
+  toast.error(text, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+};
+
+export const toastifyWarning = (text: String) => {
+  toast.warn(text, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+    transition: Bounce,
+  });
+};
