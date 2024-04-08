6587194031286d3f5e88bb883d0439d26530173a
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-04-08 19:17:42 +0800

    product list fix

--- user-fe/src/pages/productlist/index.tsx
+++ user-fe/src/pages/productlist/index.tsx
@@ -10,33 +10,35 @@ import { categoryType } from "@/types/categoryType";
 function productList({
   productData,
   subCategoryBackendData,
-  categoryData
+  categoryData,
 }: {
-  categoryData:categoryType[];
+  categoryData: categoryType[];
   productData: ProductType[];
   subCategoryBackendData: TypeSubCategory[];
 }) {
   return (
     <div className="lg:w-full flex flex-col items-center">
       <div className="lg:flex lg:gap-5 ">
-        <SubCategory subCategoryData={subCategoryBackendData} categoryData={categoryData} />
-        <Product productData={productData} />
+        <SubCategory
+          subCategoryData={subCategoryBackendData}
+          categoryData={categoryData}
+        />
+        <Product productData={productData} favProducts={[]} />
       </div>
     </div>
   );
 }
 export default productList;
 export const getServerSideProps = async () => {
-
   const productRes = await instance.get(`/getProducts`);
   const subCategoryRes = await instance.get(`/getSubCategories`);
   const categoryRes = await instance.get(`/getCategories`);
   const productData = productRes.data;
   const subCategoryBackendData = subCategoryRes.data;
   const categoryData = categoryRes.data;
-  console.log("bumaa",subCategoryBackendData)
+  console.log("bumaa", subCategoryBackendData);
 
   return {
-    props: { productData, subCategoryBackendData,categoryData },
+    props: { productData, subCategoryBackendData, categoryData },
   };
 };
