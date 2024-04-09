29867f8a40e81eeee869a90d058c49a0af2b8f1d
Lkhagvasuren Orgilsaikhan <ace@Lkhagvasurens-MacBook-Pro.local>
2024-04-09 16:31:30 +0800

    fix product.tsx problem

--- admin-fe/src/pages/product.tsx
+++ admin-fe/src/pages/product.tsx
@@ -3,7 +3,7 @@ import { TokenContext } from "@/contexts/TokenContext";
 import { instance } from "@/instance";
 import { Category } from "@/types/categoryType";
 import { GetProductType } from "@/types/getProductType";
-import { OrderType } from "@/types/orderType";
+import { OrderType, productTypeForShop } from "@/types/orderType";
 import { getOrders } from "@/utilities/getOrders";
 import { getProducts } from "@/utilities/getProducts";
 import React, { useContext, useEffect, useState } from "react";
@@ -14,13 +14,13 @@ export default function product({
   categoryData: Category[];
 }) {
   const [visibleComponent, setVisibleComponent] = useState("");
-  const [orderData, setOrderData] = useState<OrderType[]>([]);
+  const [orderData, setOrderData] = useState<productTypeForShop[]>([]);
   const { token, setToken } = useContext(TokenContext);
   const [productData, setProductData] = useState<GetProductType[]>([]);
   const setProduct = (data: GetProductType[]) => {
     setProductData(data);
   };
-  const setOrder = (data: OrderType[]) => {
+  const setOrder = (data: productTypeForShop[]) => {
     setOrderData(data);
   };
   useEffect(() => {
